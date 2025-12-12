21adbc5b65b168b282952a59afebe231cb26720d
ainan ihsan <ainanihsan@hotmail.com>
2025-12-12 09:57:34 +0100

    Upgraded the solution to .Net 8

--- InventoryManagement/Model/Product.cs
+++ InventoryManagement/Model/Product.cs
@@ -3,9 +3,8 @@
     internal class Product
     {
         public string Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } 
         public string Color { get; set; }
-        public string Description { get; set; }
         public string Category { get; set; }
         public string Size { get; set; }
     }
